54b2a3348cc723ec40390411a99f414478c1403c
ウエスリー <wesley@cipolla.evokateur.net>
2025-04-18 16:09:31 -0700

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- :tnoremap <Esc> <C-\><C-n>
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
+
+-- :vnoremap <C-r> "hy:%s/<C-r>h//gc<left><left><left>
+vim.keymap.set("v", "<C-r>", [["hy:%s/<C-r>h//gc<left><left><left>]], { noremap = true })
+
+-- :map <F3> :noh<CR>
+vim.keymap.set("", "<F3>", ":noh<CR>", { noremap = true })
+
+-- :map <C-W><C-W> :%s/\s\+$//<CR>
+vim.keymap.set("", "<C-W><C-W>", ":%s/\\s\\+$//<CR>", { noremap = true })
