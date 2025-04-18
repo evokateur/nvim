81d238717459edc9017543392b43385cc9f7abbe
ウエスリー <wesley@turnip.evokateur.net>
2025-04-17 23:21:58 -0700

    four spaces for tab stops ffs

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smarttab = true
