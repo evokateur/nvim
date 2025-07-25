1aeefd45bcdc9d0c7bc89928c442cf05dfefe02d
ウエスリー <wesley@sfizio.evokateur.net>
2025-07-25 15:31:21 -0700

    add sample config for quarto

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -5,5 +5,31 @@ return {
       "jmbuhr/otter.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+    config = function()
+      require("quarto").setup({
+        lspFeatures = {
+          languages = { "r", "python", "rust" },
+          chunks = "all",
+          diagnostics = {
+            enabled = true,
+            triggers = { "BufWritePost" },
+          },
+          completion = {
+            enabled = true,
+          },
+        },
+        keymap = {
+          hover = "H",
+          definition = "gd",
+          rename = "<leader>rn",
+          references = "gr",
+          format = "<leader>gf",
+        },
+        codeRunner = {
+          enabled = true,
+          default_method = "molten",
+        },
+      })
+    end,
   },
 }
