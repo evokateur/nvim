83f816ce2baebb443d54050311b005e80ff99210
ウエスリー <wesley@sfizio.evokateur.net>
2025-07-27 03:32:08 -0700

    point neovim at virtual env

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.python3_host_prog = "$HOME/.pyenv/shims/python3"
+vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
 
 -- I find auto open annoying, keep in mind setting this option will require setting
 -- a keybind for `:noautocmd MoltenEnterOutput` to open the output again
