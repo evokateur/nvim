fc50a5be3bda55458b13d9763612f3428ef5513c
ウエスリー <wesley@turnip.evokateur.net>
2025-08-20 15:50:46 -0700

    organize molten options

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -5,9 +5,11 @@ return {
     dependencies = { "3rd/image.nvim" },
     build = ":UpdateRemotePlugins",
     init = function()
-      -- these are examples, not defaults. Please see the readme
       vim.g.molten_image_provider = "image.nvim"
-      vim.g.molten_output_win_max_height = 40
+      vim.g.molten_auto_open_output = false -- requires setting keybind for `:noautocmd MoltenEnterOutput`
+      vim.g.molten_wrap_output = true
+      vim.g.molten_virt_text_output = true -- Output as virtual text. Allows outputs to always be shown, works with images, but can be buggy with longer images
+      vim.g.molten_virt_lines_off_by_1 = true -- make it so the output shows up below the \`\`\` cell delimiter
       vim.g.molten_enter_output_behavior = "open_and_enter"
     end,
   },
