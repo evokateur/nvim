07d2860aeb71cee834e6183aed0934ef000df729
ウエスリー <wesley@turnip.evokateur.net>
2026-01-17 09:54:29 -0800

    use prepend_args instead of args 'https://github.com/LazyVim/LazyVim/discussions/4094#discussioncomment-15525364'

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -4,11 +4,8 @@ return {
   opts = {
     linters = {
       ["markdownlint-cli2"] = {
-        args = {
-          "--config",
-          vim.fn.stdpath("config") .. "/.markdownlint-cli2.yaml",
-          "--",
-        },
+        -- https://github.com/LazyVim/LazyVim/discussions/4094#discussioncomment-15525364
+        prepend_args = { "--config", vim.fn.stdpath("config") .. "/.markdownlint-cli2.yaml", "--" },
       },
     },
   },
