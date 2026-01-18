a190e7900d14d86130e5b6bd38a800a0a6bc86f7
ウエスリー <wesley@sfizio.evokateur.net>
2026-01-17 16:11:32 -0800

    clean up comment with url, is in commit message

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -4,7 +4,6 @@ return {
   opts = {
     linters = {
       ["markdownlint-cli2"] = {
-        -- https://github.com/LazyVim/LazyVim/discussions/4094#discussioncomment-15525364
         prepend_args = { "--config", vim.fn.stdpath("config") .. "/.markdownlint-cli2.yaml", "--" },
       },
     },
