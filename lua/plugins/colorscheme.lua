595eab4943222f643ff250ec9ce24de24c3f0fec
ウエスリー <wesley@cipolla.evokateur.net>
2026-06-08 15:28:55 -0700

    update colorscheme.lua to load nightfox or dayfox according to system

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,33 @@
+local function is_dark()
+  local r = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+  local s = r:read("*l")
+  r:close()
+  return s == "Dark"
+end
+
+local function apply_theme()
+  if is_dark() then
+    vim.o.background = "dark"
+    vim.cmd.colorscheme("nightfox")
+  else
+    vim.o.background = "light"
+    vim.cmd.colorscheme("dayfox")
+  end
+end
+
 return {
   {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nightfox").setup({})
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        once = true,
+        callback = apply_theme,
+      })
+      vim.api.nvim_create_autocmd("FocusGained", { callback = apply_theme })
+    end,
   },
 }
