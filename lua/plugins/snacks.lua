22b4271f94a544e20a164597e1e24d3ede998d28
ウエスリー <wesley@cipolla.evokateur.net>
2026-06-16 13:04:44 -0700

    add .gitignore to hiddent stuff that shows up in explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,7 +2,7 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
-      include = { "*.local.*", "*.override.*", ".env", ".claude" },
+      include = { ".gitignore", ".env", "*.local.*", "*.override.*", ".claude" },
     },
   },
 }
